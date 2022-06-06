ca08c3e70ec3cbef2918e73cefccb687ece3123c
Juwon-Yun <eooll2@daum.net>
2022-06-06 16:02:16 +0900

    Feat: maps, geolocation setting test complete

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@
   지도에 마커를 표시해서 현재 위치와 거리 구하기
  */
 
-void main(){
+import 'package:flutter/material.dart';
+import 'package:go_to_wark/screen/home_screen.dart';
 
+void main() {
+  runApp(MaterialApp(home: Scaffold(body: HomeScreen())));
 }
