4003039c7087f0be9af7820668a76cbbd7919d50
Juwon-Yun <eooll2@daum.net>
2022-06-06 22:21:06 +0900

    Feat: permission state FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -6,5 +6,5 @@ import 'package:flutter/material.dart';
 import 'package:go_to_wark/screen/home_screen.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomeScreen()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen()));
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -20,37 +20,58 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:renderAppBar(),
-      body: Column(children: [
-        _CustomGoogleMap(inittialCameraPosition: inittialCameraPosition),
-        _ChollCheck()
-      ]),
+      appBar: renderAppBar(),
+      // Future Builder는 context, snapshot 두개를 받아야한다.
+      body: FutureBuilder(
+        // future를 리턴하는 함수를 쓴다.
+        // 에러 혹은 무언가 받았을때 화면을 다시 빌드해준다.
+        future: checkPermission(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          print(snapshot);
+          print(snapshot.data);
+          // none(future를 리턴하지않음), waiting(async 기다림), done(완료)
+          print(snapshot.connectionState);
+          if (snapshot.data == '위치 권한이 허가되었습니다.') {
+            return Column(children: [
+              _CustomGoogleMap(inittialCameraPosition: inittialCameraPosition),
+              _ChollCheck()
+            ]);
+          }else{
+            return Center(
+              child: Text(snapshot.data.toString()),
+            );
+          }
+        },
+      ),
     );
   }
 
   Future<String> checkPermission() async {
     final isLocationEnable = await Geolocator.isLocationServiceEnabled();
 
-    if(!isLocationEnable){
+    if (!isLocationEnable) {
       return '위치 서비스를 활성화 해주세요';
     }
 
     LocationPermission checkPermission = await Geolocator.checkPermission();
 
     // denied면 요청
-    if(checkPermission == LocationPermission.denied){
+    if (checkPermission == LocationPermission.denied) {
       checkPermission = await Geolocator.requestPermission();
 
-      if(checkPermission == LocationPermission.denied){
+      if (checkPermission == LocationPermission.denied) {
         return '위치 권한을 허가해주세요.';
       }
     }
 
-    if(checkPermission == LocationPermission.deniedForever){
+    if (checkPermission == LocationPermission.deniedForever) {
       return '앱의 위치 권한을 설정에서 허가해주세요.';
     }
 
-    // always, whileInUse
+    // enum이 always, whileInUse일 때
     return '위치 권한이 허가되었습니다.';
   }
 }
@@ -62,7 +83,7 @@ class _ChollCheck extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(flex: 1,child: Text('출근'));
+    return Expanded(flex: 1, child: Text('출근'));
   }
 }
 
@@ -85,7 +106,7 @@ class _CustomGoogleMap extends StatelessWidget {
   }
 }
 
-AppBar renderAppBar(){
+AppBar renderAppBar() {
   return AppBar(
     centerTitle: true,
     title: Text(
